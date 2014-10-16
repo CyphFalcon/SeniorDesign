1f0ec3f8bf9a3a35034c2b8b9a740638e42a703b
CyphFalcon <CyphFalcon@users.noreply.github.com>
2014-10-15 22:01:06 -0400

    Create camera.m
    Grabs original image for use in scanning.

---
+++ camera.m
@@ -0,0 +1,10 @@
+vid = videoinput('winvideo', 1, 'MJPG_640x480')
+set(vid,'TriggerRepeat',Inf);
+   vid.FrameGrabInterval = 1;
+   start(vid);
+   data = getdata(vid,1);
+   img = data(:,:,:,1);
+   image(img);
+   stop(vid);
+   
+imwrite(img,'previous.png');
