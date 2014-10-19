440ec8b25aa363732cd92ab5f074fc89c5430e2a
CyphFalcon <CyphFalcon@users.noreply.github.com>
2014-10-19 18:56:43 -0400

    Update camera.m

--- camera.m
+++ camera.m
@@ -1,6 +1,7 @@
 vid = videoinput('winvideo', 1, 'MJPG_640x480')
 set(vid,'TriggerRepeat',Inf);
    vid.FrameGrabInterval = 1;
+   pause(1);
    start(vid);
    data = getdata(vid,1);
    img = data(:,:,:,1);
