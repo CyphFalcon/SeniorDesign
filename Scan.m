e14c1426202c13fc03a2b8b5b6a10d1365e1c088
CyphFalcon <CyphFalcon@users.noreply.github.com>
2014-11-12 17:43:09 -0500

    Update Scan.m
    Updated Scan.m to match with filter2.m

--- Scan.m
+++ Scan.m
@@ -3,30 +3,33 @@ game.Colors=[0,0,0,0,0,0,0,0,0,0];
 game.key={'0', 'Empty'; '1', 'Red'; '2', 'Green'; '3', 'Yellow'; '4', 'Blue'};
 
 %Don't touch
-rrl = 130;
-rrh = 180;
-rgl = 35;
-rgh = 65;
-rbl = 70;
-rbh = 110;
-grl = 45;
-grh = 80;
-ggl = 80;
-ggh = 125;
-gbl = 60;
-gbh = 100;
+rrl = 80;
+rrh = 125;
+rgl = 25;
+rgh = 70;
+rbl = 50;
+rbh = 95;
+
+grl = 38;
+grh = 70;
+ggl = 45;
+ggh = 80;
+gbl = 48;
+gbh = 90;
+%Did not update this block (no yellow check...)
 yrl = 150;
 yrh = 210;
 ygl = 160;
 ygh = 210;
 ybl = 60;
 ybh = 100;
-brl = 50;
-brh = 90;
-bgl = 95;
-bgh = 140;
-bbl = 130;
-bbh = 180;
+
+brl = 35;
+brh = 88;
+bgl = 48;
+bgh = 100;
+bbl = 92;
+bbh = 135;
 
 %Take image of current board...
 vid = videoinput('winvideo', 1, 'MJPG_640x480');
@@ -64,57 +67,59 @@ size = length(radii);
 i = 1;
 
 while i <= size 
-    offset = radii(i)*3/4;
-    if centers(i,1) >= 160 && centers(i,1) <= 220 && centers(i,2) >=35 && centers(i,2) <= 95
+    offset = round(radii(i)*3/4);
+    if centers(i,1) >= 160 && centers(i,1) <= 220 && centers(i,2) >=30 && centers(i,2) <= 95
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(10) = valu;
     elseif centers(i,1) >= 135 && centers(i,1) <= 195 && centers(i,2) >= 138 && centers(i,2) <= 198
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(9) = valu;
     elseif centers(i,1) >= 50 && centers(i,1) <= 110 && centers(i,2) >= 225 && centers(i,2) <= 285
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(8) = valu;
     elseif centers(i,1) >= 130 && centers(i,1) <= 190 && centers(i,2) >= 300 && centers(i,2) <= 360
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(7) = valu;
     elseif centers(i,1) >= 180 && centers(i,1) <= 240 && centers(i,2) >= 410 && centers(i,2) <= 480
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(6) = valu;
     elseif centers(i,1) >= 300 && centers(i,1) <= 360 && centers(i,2) >= 75 && centers(i,2) <= 135
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(1) = valu;
     elseif centers(i,1) >= 406 && centers(i,1) <= 466 && centers(i,2) >= 68 && centers(i,2) <= 128
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(2) = valu;
-    elseif centers(i,1) >= 416 && centers(i,1) <= 476 && centers(i,2) >= 214 && centers(i,2) <= 274
+    elseif centers(i,1) >= 416 && centers(i,1) <= 476 && centers(i,2) >= 210 && centers(i,2) <= 274
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(3) = valu;
-    elseif centers(i,1) >= 420 && centers(i,1) <= 480 && centers(i,2) >= 360 && centers(i,2) <= 420
+    elseif centers(i,1) >= 420 && centers(i,1) <= 480 && centers(i,2) >= 355 && centers(i,2) <= 420
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(4) = valu;
-    elseif centers(i,1) >= 300 && centers(i,1) <= 370 && centers(i,2) >= 365 && centers(i,2) <= 430
+    elseif centers(i,1) >= 300 && centers(i,1) <= 370 && centers(i,2) >= 355 && centers(i,2) <= 430
         x = round(centers(i,1));
         y = round(centers(i,2));
-        filter;
+        filter2;
         game.Colors(5) = valu;
+    else
+        fprintf('Something unidentified at %d,%d\n',centers(i,1),centers(i,2));
     end
     i = i + 1;
 end
